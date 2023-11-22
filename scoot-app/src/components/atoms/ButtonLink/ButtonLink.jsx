ab2b8203909714112b3ae5a26378df63ff9af5e6
iuna Leon <yuna.ads@hotmail.com>
2023-11-21 22:48:24 -0300

    Hero

--- scoot-app/src/components/atoms/ButtonLink/ButtonLink.jsx
+++ scoot-app/src/components/atoms/ButtonLink/ButtonLink.jsx
@@ -3,7 +3,7 @@ import Styles from "./buttonlink.module.scss";
 export default function ButtonLink(props) {
   const ButtonClass = `${Styles.link__anchor} ${props.className} ${
     Styles[`button__Link--${props.typography}`]
-  } ${Styles[`button__Link--${props.position}`]}`;
+  } `;
   return (
     <a className={ButtonClass} href="/button">
       {props.label}
--- scoot-app/src/components/organisms/Hero/Hero.jsx
+++ scoot-app/src/components/organisms/Hero/Hero.jsx
@@ -4,20 +4,20 @@ import SectionTitle from "../../atoms/SectionTitle/SectionTitle";
 import Styles from "./hero.module.scss";
 import arrow from "../../../assets/img/Home/arrow.png";
 import arrowTablet from "../../../assets/img/Home/arrowTablet.png";
+import arrowDesk from "../../../assets/img/Home/arrowDesk.png";
 import balls from "../../../assets/img/Home/balls.png";
 import line from "../../../assets/img/Home/line.png";
 
 export default function Hero() {
   return (
     <div className={Styles.container}>
-      <div className={Styles.container__allcomponts}>
-        <div className={Styles.container__alltext}>
-          <SectionTitle
-            className={`${Styles.container__title}`}
-            typography={"h3"}
-            title={"Scooter sharing made simple"}
-          />
-
+      <div className={Styles.container__alltext}>
+        <SectionTitle
+          className={`${Styles.container__title}`}
+          typography={"h3"}
+          title={"Scooter sharing made simple"}
+        />
+        <div className={Styles.container__textBnt}>
           <SectionText
             className={`${Styles.container__paragraph}`}
             typography={"body"}
@@ -25,17 +25,18 @@ export default function Hero() {
               "Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient locations in each of our cities. Use our app to locate the nearest bike, unlock it with a tap, and you’re away!"
             }
           />
+          <ButtonLink
+            className={`${Styles.container__btnLink}`}
+            typography={"button"}
+            label={"Get Scootin"}
+          />
         </div>
-
-        <ButtonLink
-          className={`${Styles.container__btnLink}`}
-          typography={"button"}
-          label={"Get Scootin"}
-        />
       </div>
+
       <picture className={Styles.container__picture}>
         <img className={Styles.container__arrow} src={arrow} />
         <img className={Styles.container__arrowTablet} src={arrowTablet} />
+        <img className={Styles.container__arrowDesk} src={arrowDesk} />
         <img className={Styles.container__balls} src={balls} />
         <img className={Styles.container__line} src={line} />
       </picture>
